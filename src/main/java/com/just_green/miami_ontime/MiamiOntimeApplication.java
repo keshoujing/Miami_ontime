b42db9745c9f05f6fb09735f1a27032222ef9685
ske@ufl.edu <ske@ess-tra-40454.essie.ufl.edu>
2023-05-25 11:53:42 -0400

    first commit

---
+++ src/main/java/com/just_green/miami_ontime/MiamiOntimeApplication.java
@@ -0,0 +1,13 @@
+package com.just_green.miami_ontime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiamiOntimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiamiOntimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/just_green/miami_ontime/MiamiOntimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.just_green.miami_ontime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiamiOntimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
