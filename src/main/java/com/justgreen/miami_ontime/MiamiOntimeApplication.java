51577ed079b6e4b14ca04394d76ccefe6718a126
keshoujing <keshoujing@gmail.com>
2023-05-25 12:45:10 -0400

    first commit

---
+++ src/main/java/com/justgreen/miami_ontime/MiamiOntimeApplication.java
@@ -0,0 +1,16 @@
+package com.justgreen.miami_ontime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MiamiOntimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiamiOntimeApplication.class, args);
+        System.out.println("test");
+    }
+
+}
---
+++ src/test/java/com/justgreen/miami_ontime/MiamiOntimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justgreen.miami_ontime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiamiOntimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
